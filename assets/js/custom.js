baa65ca462d5e350c1db376fa5f0dfbfbb50a124
Afolabi Sobayo <sobayoa@gmail.com>
2020-08-03 23:19:57 +0100

    new uds

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,49 +1,8 @@
-         var navMenuDiv = document.getElementById("nav-content");
-         var navMenu = document.getElementById("nav-toggle");
-         
-         var helpMenuDiv = document.getElementById("menu-content");
-         var helpMenu = document.getElementById("menu-toggle");
-         
-         document.onclick = check;
-         
-         function check(e){
-           var target = (e && e.target) || (event && event.srcElement);
-         
-          
-           //Nav Menu
-           if (!checkParent(target, navMenuDiv)) {
-
-         	if (checkParent(target, navMenu)) {
-         	  if (navMenuDiv.classList.contains("hidden")) {
-         		navMenuDiv.classList.remove("hidden");
-         	  } else {navMenuDiv.classList.add("hidden");}
-         	} else {
-         	  navMenuDiv.classList.add("hidden");
-         	}
-           }
-           
-           //Help Menu
-           if (!checkParent(target, helpMenuDiv)) {
-         	if (checkParent(target, helpMenu)) {
-         	  if (helpMenuDiv.classList.contains("hidden")) {
-         		helpMenuDiv.classList.remove("hidden");
-         	  } else {helpMenuDiv.classList.add("hidden");}
-         	} 
-           }
-         }
-         
-         function checkParent(t, elm) {
-           while(t.parentNode) {
-         	if( t == elm ) {return true;}
-         	t = t.parentNode;
-           }
-           return false;
-         }
-
-       /*Toggle dropdown list*/
-       function toggleDD(myDropMenu) {
-        document.getElementById(myDropMenu).classList.toggle("invisible");
-    }   
+$(document).ready(function(){
+  if(window.innerWidth < 640){
+    $('.oddslide-mobile').removeClass('oddslide');
+  }
+});
          
    /** Odds slider**/
    $(document).ready(function(){
@@ -368,48 +327,13 @@ $('.adslided').slick({
                         // settings: "unslick"
                         // instead of a settings object
                       ]
-                    });   
+                    });                     
 
-         
-         // Open modal:            
 
-                    var openmodal = document.querySelectorAll('.modal-open')
-                    for (var i = 0; i < openmodal.length; i++) {
-                      openmodal[i].addEventListener('click', function(event){
-                      event.preventDefault()
-                      toggleModal()
-                      })
-                    }
-                    
-                    const overlay = document.querySelector('.modal-overlay')
-                    overlay.addEventListener('click', toggleModal)
-                    
-                    var closemodal = document.querySelectorAll('.modal-close')
-                    for (var i = 0; i < closemodal.length; i++) {
-                      closemodal[i].addEventListener('click', toggleModal)
-                    }
-                    
-                    document.onkeydown = function(evt) {
-                      evt = evt || window.event
-                      var isEscape = false
-                      if ("key" in evt) {
-                      isEscape = (evt.key === "Escape" || evt.key === "Esc")
-                      } else {
-                      isEscape = (evt.keyCode === 27)
-                      }
-                      if (isEscape && document.body.classList.contains('modal-active')) {
-                      toggleModal()
-                      }
-                    };
-                    
-                    
-                    function toggleModal () {
-                      const body = document.querySelector('body')
-                      const modal = document.querySelector('.modal')
-                      modal.classList.toggle('opacity-0')
-                      modal.classList.toggle('pointer-events-none')
-                      body.classList.toggle('modal-active')
-                    }
+                  }); 
 
-  }); 
+ 
+
+ 
+
    
