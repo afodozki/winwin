77571e119b3f80f936578d4adb2f3140b353c396
Afolabi Sobayo <sobayoa@gmail.com>
2020-08-04 13:06:35 +0100

    padd fix

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -93,6 +93,51 @@ $(document).ready(function(){
       ]
     });
 
+//loginslide
+$('.loginslide').slick({
+  infinite: true,
+  speed: 900,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  dots: true,
+  arrows:false,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        arrows:false,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows:false,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        arrows:false,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+
+
 
 //adslided
 $('.adslided').slick({
