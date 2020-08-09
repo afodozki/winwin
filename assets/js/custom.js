bd9ab0ca63d5a9abf7b828be573407d6a5abe56e
Afolabi Sobayo <sobayoa@gmail.com>
2020-08-09 20:00:34 +0100

    padd fix

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -405,6 +405,57 @@ $('.adslided').slick({
                     });                     
 
 
+
+
+//tierslided
+$('.tierslide').slick({
+  infinite: true,
+  speed: 900,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  centerMode: true,
+  centerPadding: '100px',
+  dots: false,
+  arrows:false,
+  autoplay: true,
+  autoplaySpeed: 2500,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        arrows:false,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        dots: true,
+        infinite: true,
+        arrows:false,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        infinite: true,
+        arrows:false,
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});                    
                   }); 
 
  
